1b7811087adb5b3c2c3fab47638f5c27be161e64
codingmatemoon <abc9529@naver.com>
2023-02-28 01:04:04 +0900

    ItemRepository 개발 - 새로 생성한 객체라 id가 없는 경우 persist, 있는 경우 기존 객체로 보고 merge로 데이터를 수정합니다.

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            /*
+            item 새로 생성한 객체이기 때문에 처음에는 id값이 없어서 persist를 통해 신규로 등록합니다.
+             */
+            em.persist(item);
+        } else {
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}

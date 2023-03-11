65472baa74632bc37b725b7eaf6321621f6e9763
codingmatemoon <abc9529@naver.com>
2023-03-11 09:50:15 +0900

    상품 등록 기능 추가

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        /* setter를 쓰기 보다 create book 등 생성자 메서드를 활용하여 만들고 넘겨주는 것이 좋습니다.*/
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}

0b2f98681a89404a5b83ad82b3073136cb31ee1a
codingmatemoon <abc9529@naver.com>
2023-03-13 01:03:40 +0900

    상품 수정 기능 추가 - @PostMapping("items/{itemId}/edit") // id가 조작돼서 넘어올 경우 다른 사람것이 수정될 수 있습니다. 이러한 취약점을 해결하기 위해 이 유저가 이 아이템에 대해 권한이 있는지 서버에서 체크하는 로직, 업데이트할 객체를 세션에 담아두고 풀어내는 방법 등이 있습니다.

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -25,7 +27,7 @@ public class ItemController {
 
     @PostMapping("/items/new")
     public String create(BookForm form) {
-        /* setter를 쓰기 보다 create book 등 생성자 메서드를 활용하여 만들고 넘겨주는 것이 좋습니다.*/
+        /* setter를 쓰기 보다 create book 등    생성자 메서드를 활용하여 만들고 넘겨주는 것이 좋습니다.*/
         Book book = new Book();
         book.setName(form.getName());
         book.setPrice(form.getPrice());
@@ -42,5 +44,36 @@ public class ItemController {
         List<Item> items = itemService.findItems();
         model.addAttribute("items", items);
         return "items/itemList";
+    }
+
+    @GetMapping("items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    // id가 조작돼서 넘어올 경우 다른 사람것이 수정될 수 있습니다. 이러한 취약점을 해결하기 위해 이 유저가 이 아이템에 대해 권한이 있는지 서버에서 체크하는 로직, 업데이트할 객체를 세션에 담아두고 풀어내는 방법 등이 있습니다.
+    @PostMapping("items/{itemId}/edit")
+    public String updateItem(@PathVariable String itemId, @ModelAttribute("form") BookForm form) {
+        Book book= new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
     }
 }
